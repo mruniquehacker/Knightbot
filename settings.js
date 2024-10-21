912b7e075d86483fb627a896b87663cd5eca4c60
SteamaACC01 <ssteamacc01@gmail.com>
2024-10-21 08:12:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'My Sticker Pack',
   author: 'My Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'WITCHER', // Your name
+  ownerNumber: '94725337377', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
