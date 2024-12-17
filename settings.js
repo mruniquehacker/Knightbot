f0a4673528d4684e2cc082ed1fe97e6368c6057f
Seongyoung123123 <Kumela281@gmail.com>
2024-12-18 02:50:02 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'My Sticker Pack',
   author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "𓆩❤𓆪👨💓👩𝘾𝙃𝙐𝙕𝙕𝘼𝘼𓆩❤𓆪👨💓👩",
+  botOwner: 'AHMED', // Your name
+  ownerNumber: '923253980306', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
